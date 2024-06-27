ecd0911698b45ed9a56ca0a6cd983acd45b1f96d
Mukul Ingle <mukul@metashopai.com>
2024-06-27 20:18:43 +0530

    Disabled zoom

--- src/OrbitControls.js
+++ src/OrbitControls.js
@@ -80,7 +80,7 @@ class OrbitControls extends EventDispatcher {
 
         // This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
         // Set to false to disable zooming
-        this.enableZoom = true;
+        this.enableZoom = false;
         this.zoomSpeed = 1.0;
 
         // Set to false to disable rotating
@@ -1414,3 +1414,4 @@ class OrbitControls extends EventDispatcher {
 }
 
 export { OrbitControls };
+
